8aacba776fa61202a3cd2a78bb6bef8f4da893ea
myjdml <myjdml_codemaster@163.com>
2020-09-17 18:36:43 +0800

    first create

---
+++ .history/vue.config_20200917183637.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: Code_Master
+ * @Date: 2020-09-17 18:34:17
+ * @LastEditTime: 2020-09-17 18:36:37
+ * @LastEditors: Code_Master
+ * @FilePath: /privacy-policy/vue.config.js
+ * @Description: The first 90% of the code accounts for the first 90% of the development time. The remaining 10% of the code accounts for the other 90% of the development time.
+ */
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production' ? '/game/privacy-policy/' : './',
+    outputDir: 'build',
+  };
