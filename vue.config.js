683ab935d1400f465cdafeeddfbf14c224612a66
myjdml <898762316@qq.com>
2021-05-24 15:08:22 +0800

    fix: 修改yaml

--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,6 @@
  * @Description: The first 90% of the code accounts for the first 90% of the development time. The remaining 10% of the code accounts for the other 90% of the development time.
  */
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/game/privacy-policy/' : './',
+    publicPath: process.env.NODE_ENV === 'production' ? '/privacy-policy/' : './',
     outputDir: 'build',
   };
